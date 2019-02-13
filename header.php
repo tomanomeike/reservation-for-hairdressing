8ace9450479c86d6dbd6a51c46527a0673c1015f
LAPTOP-D77I3KMJ\toman <tomanom@gmail.com>
2019-02-13 10:15:12 +0200

    start

---
+++ header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+    <title>Hair Style</title>
+</head>
+<body>
+<header>
+    <!-- Image and text -->
+    <nav class="navbar">
+        <a class="navbar-brand" href="#">
+            <img src="C:\xampp\htdocs\www\nfq\img" width="30" height="30">
+        </a>
+
+    </nav>
+</header>
